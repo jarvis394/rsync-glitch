0b17889455957e33ea9071bcbb2d9fc43dc8d7a0
Glitch (hello-express) <none>
2020-01-12 11:57:55 +0000

    👸🌍 Checkpoint ./src/index.js:1424713/443 ./package.json:1424713/5

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 const cli = require('commander')
+const chalk = require('chalk')
 const rsync = require('rsync')
 const http = require('http')
 const options = require('./options')
+const packageJson = require('../package.json')
 
 /**
  * HTTP server for simply listen on some port in order to
  * stop Glitch app showing 'loading' icon
  */
 const server = http.createServer()
+
+/** Describes an application. Short and simple. */
+const appDescription = chalk.bold('Copies contents of your project to the external server 🚀')
+
+cli
+  .name(packageJson.name)
+  .version(packageJson.version)
+  .description(appDescription)
 
 /** Set up options for cli application */
 options.forEach(({ option, description, defaultValue, required }) => {
