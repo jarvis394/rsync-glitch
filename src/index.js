2af77c5d71c33f6dd0b842ab07ee028b0aa3ebff
Glitch (hello-express) <none>
2020-01-12 12:44:09 +0000

    💍☕️ Checkpoint ./src/options.js:1424713/4 ./src/ignoredList.js:1424713/88 ./src/index.js:1424713/608 ./watch.json:1424713/4

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const rsync = require('rsync')
 const http = require('http')
 const chokidar = require('chokidar')
 const options = require('./options')
+const ignoredList = require('./ignoredList')
 const packageJson = require('../package.json')
 
 /**
@@ -36,6 +37,15 @@ if (cli.listen) {
   console.log(chalk.green('Your app is listening on port'), chalk.yellow(cli.listen))
 }
 
-chokidar.watch('./src', { ignoreInitial: true }).on('all', (event, path) => {
-  console.log(event, path);
-});
+const watcher = chokidar.watch('./src', { 
+  ignoreInitial: true,
+  ignore: ignoredList
+})
+  
+watcher
+  .on('change', (path, stats) => {
+    console.log(path, stats)
+  })
+  .on('raw', (event, path, details) => { // internal
+    console.log('Raw event info:', event, path, details);
+  })
